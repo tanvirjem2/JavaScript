441b10345469c7532938317c353d21c8d1966e99
tanvirjem2 <tanviranjum046@gmail.com>
2025-10-18 10:42:52 +0600

    First commit

---
+++ lesson_1_&_2.js
@@ -0,0 +1,155 @@
+// console.log("Hello JavaScript");
+
+// Example - 1
+
+const weight = 38;
+// const price = 760;
+const year = 1865;
+const balance = 2000;
+// const age = 14;
+
+// --- Practice - 1 ---
+
+const birthYear = 2002;
+const brothersAndSisters = 3;
+const student = 40;
+const marks = 3.24;
+const rooms = 3;
+const hours = 4;
+// const total = 25; // phone number = 01315552444
+
+// console.log(403);
+
+const money = 835;
+// console.log(money);
+
+// --- Practice - 2 ---
+
+const age = 23;
+// console.log(age);
+
+const booksToRead = 100;
+// console.log(booksToRead);
+
+const wakeUpTime = 5.00;
+// console.log(wakeUpTime);
+
+const wastedTime = 5;
+// console.log(wastedTime);
+
+const borrow = 0;
+// console.log(borrow);
+
+const goals = 4;
+// console.log(goals);
+
+const pillow = 4;
+// console.log(pillow);
+
+// const name = "Tanvir";
+
+// const address = "Rajshahi";
+
+const favFood = 'Biryani';
+
+const hidingPlace = 'Bed er niche';
+
+const drinking = 'Tea';
+const subject = 'English';
+const teacher = 'Dragon Delwar';
+const phone = 'samsung';
+const fruit = 'watermelon';
+
+// --- practice - 3 ---
+
+const name = "Tanvir";
+// console.log(name);
+
+const address = 'Rajshahi';
+// console.log(address);
+
+const universityName = 'SUST';
+// console.log(universityName);
+
+const fan = 'Ammu';
+// console.log(fan);
+
+const movieOfTheYear = 'The Equalizer';
+// console.log(movieOfTheYear);
+
+const hobby = 'Playing Game';
+// console.log(hobby);
+
+const color = 'Black';
+// console.log(color);
+
+
+// ----------------------------------------------
+
+
+const moneyMoved = true;
+
+// --- Example - 4 ---
+
+const learning = true;
+const isChilling = true;
+const isHomeworkDone = false;
+const isCrushReplied = false;
+const hasPoneBattery = true;
+const singleForever = true;
+// const passed = false;
+
+
+const isRich = true;
+const passed = false;
+// console.log(isRich);
+// console.log(passed);
+
+// --- Practice - 4 ---
+
+// const isHappy = false;
+// console.log(isHappy);
+
+const gfMessage = true;
+// console.log(gfMessage);
+
+const isHappy = true;
+// console.log(isHappy);
+
+const hasMoney = false;
+// console.log(hasMoney);
+
+const isHungry = true;
+// console.log(isHungry);
+
+const isStudyDone = true;
+// console.log(isStudyDone);
+
+const isPhoneCharged = true;
+// console.log(isPhoneCharged);
+
+const isDinnerDone = true;
+// console.log(isDinnerDone);
+
+// ------------------------------------
+
+const eggPrice = 10;
+const onionPrice = 20;
+const total = eggPrice ** onionPrice;
+// console.log(total);
+
+
+// console.log(10 % 3);
+
+const price = 35;
+const inboxEAsenPrice = price + 10;
+const newPrice = price + 50;
+const discountedPrice = newPrice - 10;
+// console.log(discountedPrice)
+
+// --- Practice - 5 ---
+
+const earnMoney = 100;
+const useMoney = 40;
+const theMoneyIHave = earnMoney - useMoney;
+console.log(theMoneyIHave);
