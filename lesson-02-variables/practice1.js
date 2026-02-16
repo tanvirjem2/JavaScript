7bbf3b469bdcb438338ee85de7176bdd7ab1b97f
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-17 03:39:20 +0600

    Practice - 1 (L2)

---
+++ lesson-02-variables/practice1.js
@@ -0,0 +1,24 @@
+// Example - 1
+
+// Hints: We need 5 things to declare the variable. 
+// Those are ({const, let, var}, name, equal sign, value, semi-colon).
+
+const weight = 38;
+const price = 760;
+const year = 1865;
+const balance = 2000;
+const age = 14;
+
+// --- Practice - 1 ---
+// --- Total - 7 Problem ---
+
+const birthYear = 2002;
+const brotherAndSisters = 3;
+const students = 38;
+// --- Last Semester CGPA ---
+const marks = 3.25;
+const rooms = 4;
+const time = "3 hr";
+// My phone number - 01315552444
+const total = 0 + 1 + 3 + 1 + 5 + 5 + 5 + 2 + 4 + 4 + 4;
+
---
+++ lesson3.js
@@ -0,0 +1,82 @@
+// const passed = false;
+// console.log(typeof passed);
+
+const country = 'Bangladesh';
+const passed = '+';
+// const price = 555;
+
+// console.log(typeof country);
+// console.log(typeof passed);
+// console.log(typeof price);
+
+// --- Practice - 5 ---
+
+// const price = 555;
+// console.log(typeof price);
+
+const learning = true;
+// console.log(typeof learning);
+
+const megaCity = "Dhaka"
+// console.log(typeof megaCity);
+
+const temperature = 102.5
+// console.log(typeof temperature);
+
+const isHungry = true;
+// console.log(typeof isHungry);
+
+const isRich = true;
+// console.log(typeof isRich);
+
+// --- Let ---
+
+// --- Example - 1 ---
+
+let price = 35;
+price = 45;
+// console.log(price);
+
+// --- Example - 2 ---
+
+let balance = 500;
+balance = 400;
+// console.log(balance);
+
+// --- Example - 3 ---
+
+// const name = "Kala Mia"
+// name = "Lal Mia"
+// console.log(name);
+
+// --- Practice 6 ---
+
+let age = 24;
+// console.log(age);
+age = 25;
+// console.log(age);
+
+let pocketMoney = 500;
+pocketMoney = 150;
+// console.log(pocketMoney);
+
+let studyTime = 4;
+studyTime = 0;
+// console.log(studyTime);
+
+let friendsCount = 3;
+friendsCount = friendsCount - 2;
+// console.log(friendsCount);
+
+let phonePrice = 25000;
+phonePrice = 12000;
+// console.log(phonePrice);
+
+let chairsCount = 40;
+chairsCount = chairsCount - 2;
+chairsCount = chairsCount - 6;
+console.log(chairsCount);
+
+let playTime = 3;
+playTime = 0;
+console.log(playTime)
