6727f19440a4edcf21f5a633be49c7b4c2384780
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-18 02:36:42 +0600

    Practice - 4 (L2)

---
+++ lesson-02-variables/practice4.js
@@ -0,0 +1,22 @@
+// --- Practice - 4 ---
+// --- Total - 7 Problem (Solved) ---
+
+const isFriendHappy = false;
+console.log(isFriendHappy);
+
+const crushReply = true;
+console.log(crushReply);
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+
+const isStudyDone = true;
+console.log(isStudyDone);
+
+const isPhoneCharged = true;
+console.log(isPhoneCharged);
+
+const isDinnerDone = true;
+console.log(isDinnerDone);
