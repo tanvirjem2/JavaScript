7c42e9b5e08446102d4239a56fdaae378c1de733
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-18 01:41:53 +0600

    Practice - 3 (L2)

---
+++ lesson-02-variables/practice3.js
@@ -0,0 +1,27 @@
+const favFood = "Biryani"
+
+const hidingPlace = 'Bed er niche';
+
+// --- Practice - 3 ---
+// --- Total - 7 Problem (Solved) ---
+
+const name = 'MD Tanvir Anjum Shozib';
+console.log(name);
+
+const address = 'Rajshahi';
+console.log(address);
+
+const university = 'SUST';
+console.log(university);
+
+const fan = 'sunny leon';
+console.log(fan);
+
+const movieOfTheYear = 'ABC';
+console.log(movieOfTheYear);
+
+const hobby = 'Guitar';
+console.log(hobby);
+
+const color = 'Black';
+console.log(color);
