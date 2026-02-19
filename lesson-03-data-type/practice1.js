cfd46a6553e7892276e81638e64689b30558b8e4
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-20 00:54:05 +0600

    Practice - 1 (L3)

---
+++ lesson-03-data-type/practice1.js
@@ -0,0 +1,20 @@
+// --- Practice - 1 ---
+// --- Problem 6 (Solved) ---
+
+const price = 555;
+console.log(typeof price);
+
+const learning = true;
+console.log(typeof learning);
+
+const megaCity = 'Dhaka';
+console.log(typeof megaCity);
+
+const temperature = 102.5;
+console.log(typeof temperature);
+
+const isHungry = true;
+console.log(typeof isHungry);
+
+const isRich = true;
+console.log(typeof isRich)
