5f592464cce56e18abdf90ec6edb248542edafdd
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-19 06:13:54 +0600

    Practice - 5 (L2)

---
+++ lesson-02-variables/practice5.js
@@ -0,0 +1,60 @@
+// --- Practice - 5 ---
+// --- Total - 10 Problem (Solved) ---
+
+const earn = 100;
+const spend = 40;
+const total = earn - spend;
+console.log(total);
+
+const pencil = 10;
+const pencilUsedEachTime = 2;
+const howManyTimesPencilUsed = 10 / 2;
+console.log(howManyTimesPencilUsed);
+
+const iHave = 60;
+const EachTimeISpend = 15;
+const howManyTimeICanSpend = 60 / 15;
+console.log(howManyTimeICanSpend);
+const reminder = iHave % EachTimeISpend;
+console.log(reminder);
+
+const word1 = 'Hello';
+const word2 = 'World';
+const totalWord = word1 + word2;
+console.log(totalWord);
+
+const rice = 153;
+const lentil = 261;
+const totalRiceLentil = rice + lentil;
+console.log(totalRiceLentil);
+
+const iHave1 = 500;
+const EachTimeISpend1 = 75;
+const howManyTimeICanSpend1 = 500 / 75;
+console.log(howManyTimeICanSpend1);
+const reminder1 = iHave1 % EachTimeISpend1;
+console.log(reminder1);
+
+const num1 = 8;
+const num2 = 3;
+const division = num1 / num2;
+console.log(division);
+const reminder2 = num1 % num2;
+console.log(reminder2);
+
+const number1 = 50;
+const number2 = 9;
+const reminder3 = 50 % 9;
+console.log(reminder3);
+
+const firstWord = 'Bangla';
+const secondWord = 'desh';
+const totalName = firstWord + secondWord;
+console.log(totalName);
+
+const totalAmountRice = 480;
+const iCanEat = 4;
+const days = totalAmountRice / 4;
+console.log(days);
+const month = days / 30;
+console.log(month)
