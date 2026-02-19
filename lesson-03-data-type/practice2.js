f4774b3dea361cdc42775542abcaeffc2de8b814
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-20 01:47:58 +0600

    Practice - 2 (L3)

---
+++ lesson-03-data-type/practice2.js
@@ -0,0 +1,32 @@
+// --- Practice - 2 ---
+// --- Problem 7 (Solved) ---
+
+let age = 23;
+console.log(age);
+age = 24;
+console.log(age);
+
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+let studyTime = 4;
+studyTime = 2;
+console.log(studyTime);
+
+let friendsCount = 3;
+friendsCount = friendsCount - 2;
+console.log(friendsCount);
+
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+let chairCount = 40;
+chairCount = chairCount - 2;
+chairCount = chairCount - 6;
+console.log(chairCount);
+
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
