5d7f5ef5afdc0c4e4392638af7f17f985152dd45
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-20 17:48:53 +0600

    Practice - 4 (L4)

---
+++ lesson-03-data-type/practice4.js
@@ -0,0 +1,27 @@
+// --- Practice - 4 ---
+// --- Problem 7 (Solved) ---
+
+const favFood = 'Biryani';
+
+const city = 'Chittagong';
+const City = 'Dhaka';
+console.log(city, City);
+
+const userName = 'Johan';
+const username = 'Jovan';
+console.log(userName, username);
+
+const homeAddress = 'Rajshahi';
+console.log(homeAddress);
+
+// const super = 20;
+// console.log(super);
+
+// const class = '12';
+// console.log(class);
+
+// --- Can not Start the name with numbers ---
+// --- Remove the number from first and send to end of the name ---
+// const 2ndPosition = 'Tanvir';
+const Position2nd = 'Tanvir';
+console.log(Position2nd);
