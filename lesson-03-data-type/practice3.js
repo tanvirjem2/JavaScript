16d12e6257edb532d4337ec0206668c4af651d39
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-20 17:20:08 +0600

    Practice - 3 (L3)

---
+++ lesson-03-data-type/practice3.js
@@ -0,0 +1,47 @@
+// --- Practice - 3 ---
+// --- Problem 7 (Solved) ---
+
+// --- Cricket Score ---
+const score = 150;
+
+/*
+    Circle Radius
+    Normal Circle
+    In th book
+*/
+const radius = 50;
+
+/*
+    I have 10 dress
+    4 = pants
+    4 = shirts
+    2 = t-shirts
+*/
+const dress = 10;
+
+/*
+    For my Birthday
+    My Father give me 100
+*/
+const giftMoney = 100;
+
+/*
+    2 hr = Programming Book
+    3 hr = Math
+    2 hr = GK
+*/
+const studyTime = 7;
+
+/*
+    I send 100 TK for my GF
+    if she tell something bad.
+    charge 5 tk
+*/
+const sendMoney = 100;
+
+/*
+    Monitor MSI Ryans Computer Ltd.
+    Monitor Price
+    Delivery Fee = 120 tk
+*/
+const pay = 12500;
