e574246b262e7c252505f6ecf20154a54be72355
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-24 02:26:18 +0600

    Practice - 1 (L5)

---
+++ lesson-05-if-else/practice1.js
@@ -0,0 +1,46 @@
+// --- Practice - 1 ---
+// --- Problem 6 (Solved) ---
+
+const myReportingTime = 6;
+if (myReportingTime < 6 || myReportingTime === 6) {
+    console.log("My mom give me breakfast")
+} else {
+    console.log("My mom beat me")
+}
+
+const myLoginStatus = false;
+const loginSuccessful = true;
+const loginUnsuccessful = false;
+if (myLoginStatus === loginSuccessful) {
+    console.log("Welcome!");
+} else {
+    console.log("Get Lost!");
+}
+
+const myRunningDistance = 4;
+if (myRunningDistance >= 5) {
+    console.log("Chocolate");
+} else {
+    console.log("You get fat");
+}
+
+const myMarks = 85;
+if (myMarks > 80) {
+    console.log("Give me bike");
+} else {
+    console.log("No bike, Get Lost!");
+}
+
+const movieShowTime = 8;
+if (movieShowTime < 9) {
+    console.log("Watch The Movie");
+} else {
+    console.log("I go for sleep")
+}
+
+const temperature = 25;
+if (temperature >= 30) {
+    console.log("Turn on the AC");
+} else {
+    console.log("Use Blanket for sleep");
+}
