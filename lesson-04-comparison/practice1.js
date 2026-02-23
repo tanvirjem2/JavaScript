fce8f2815b7717a0ee94e5385d0da064b04202c5
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-23 08:26:13 +0600

    Practice - 1 (L4)

---
+++ lesson-04-comparison/practice1.js
@@ -0,0 +1,77 @@
+// --- Practice - 1 ---
+// --- Problem 7 (Solved) ---
+
+// ---------------------------------------------------
+
+const brother1 = 7;
+const brother2 = 5;
+
+if (brother1 > brother2) {
+    console.log("Brother 1 Eat the most sandwich");
+} else {
+    console.log("Brother 2 Eat the most sandwich")
+}
+
+// ---------------------------------------------------
+
+const class1 = 45;
+const class2 = 50;
+
+if (class1 > class2) {
+    console.log("class1 have more student then class2")
+} else {
+    console.log("class2 have more student then class1")
+}
+
+// ---------------------------------------------------
+
+const myMark = 75;
+const myFriendMark = 75;
+
+if (myMark > myFriendMark) {
+    console.log("I win");
+} else if (myMark < myFriendMark) {
+    console.log("My friend win");
+} else {
+    console.log('The marks are equal');
+}
+
+// ---------------------------------------------------
+
+const myTarget = 100;
+const myMoney = 95;
+
+if (myTarget > myMoney) {
+    console.log('You loose, you need more money');
+} else if (myMoney > myTarget) {
+    console.log("You did it, Congo...");
+} else {
+    console.log("You have the equal amount");
+}
+
+// ---------------------------------------------------
+
+if (10 != 7) {
+    console.log("Yes, They are no equal");
+} else {
+    console.log("They are equal");
+}
+
+// ---------------------------------------------------
+
+console.log(20 <= 15);
+
+// ---------------------------------------------------
+
+const myExamMark = 45;
+const myFriendExamMark = 97;
+
+if (myExamMark > myFriendExamMark) {
+    console.log("I get the more number");
+} else if (myFriendExamMark > myExamMark) {
+    console.log("My Friend get the more number");
+} else {
+    console.log("both are equal");
+}
+
+// ---------------------------------------------------
