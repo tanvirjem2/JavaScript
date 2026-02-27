e08f232931cb4206501a585710998349a3fd31b0
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-27 10:21:04 +0600

    Practice - 2 (L5)

---
+++ lesson-05-if-else/practice2.js
@@ -0,0 +1,74 @@
+// --- Practice - 2 ---
+// --- Problem 9 (Solved) ---
+
+const myAge = 25;
+const myHeight = 60;
+if (myAge > 18 && myHeight > 60) {
+    console.log("You push the car")
+} else {
+    console.log("You will sit down")
+}
+
+const myMathMarks = 90;
+const myEnglishMarks = 90;
+if (myMathMarks > 80 && myEnglishMarks > 85) {
+    console.log("You can get admission in the college");
+} else {
+    console.log("You will get married");
+}
+
+const myGPA = 5;
+const myFamilyMonthlyIncome = 9000;
+if (myGPA === 5 && myFamilyMonthlyIncome < 10000) {
+    console.log("You will get scholarship");
+} else {
+    console.log("You have to pay tuition fees");
+}
+
+const candidateAge = 29;
+const candidateExperience = 3;
+if (candidateAge < 30 && candidateExperience > 2) {
+    console.log("You can Take part in the exam");
+} else {
+    console.log("You are not eligible for the exam");
+}
+
+const myEggs = 13;
+const myChicken = true;
+if (myEggs > 12 && myChicken === true) {
+    console.log("Now I will make an Egg curry");
+} else {
+    console.log("I want to eat bread and banana");
+}
+
+const humanTemperature = 98.6;
+const cough = true;
+if (humanTemperature > 100 && cough === true) {
+    console.log("You should see a doctor");
+} else {
+    console.log("Kombol muri diye thako");
+}
+
+const attendance = 80;
+const assignment = true;
+if (attendance > 80 && assignment === true) {
+    console.log("Passed")
+} else {
+    console.log("Failed")
+}
+
+const homeElectricityGone = true;
+const mobileCharge = false;
+if (homeElectricityGone === true && mobileCharge === false) {
+    console.log("Study");
+} else {
+    console.log("Play Games");
+}
+
+const shirtPrice = 500;
+const couponCode = true;
+if (shirtPrice > 1000 && couponCode === true) {
+    console.log("You will get 20% discount");
+} else {
+    console.log("You will not get any discount");
+}
