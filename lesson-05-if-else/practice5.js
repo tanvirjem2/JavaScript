9b313e812382a1ebae23de04a2dd2c1425613571
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-28 15:50:06 +0600

    Practice - 5 (L5)

--- lesson-05-if-else/practice5.js
+++ lesson-05-if-else/practice5.js
@@ -1,2 +1,24 @@
 // --- Practice - 5 ---
 // --- Problem 7 (Solved) ---
+
+const shopping = 4000;
+const cashback = 500;
+shopping > 3000 ? console.log("You get a discount: " + (shopping - cashback)) : console.log("Pay the Full price: " + shopping);
+
+const age = 14;
+age > 15 ? console.log("Teenager") : console.log("Child");
+
+const isLoggedIn = true;
+isLoggedIn === true ? console.log("Welcome back") : console.log("Please login");
+
+const oilTank = true;
+oilTank === true ? console.log("Ready for long drive") : console.log("Fill the tank");
+
+const passExam = true;
+passExam === true ? console.log("Party time") : console.log("Next Semester 2 serious study korbo");
+
+const sunny = false;
+sunny === false ? console.log("Stay home") : console.log("Hello");
+
+const expensive = false;
+expensive === false ? console.log("I will buy this item") : console.log("I will not buy this item");
