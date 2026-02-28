38b14eaf8dcc72adf165db2288c9a8582fa8dca5
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-28 17:19:28 +0600

    Practice - 1 (L6)

---
+++ lesson-06-array/practice1.js
@@ -0,0 +1,20 @@
+// --- Practice - 1 ---
+// --- Problem - 7 (Solved) ---
+
+const num1 = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+
+const smallFreeze = ['Apple', 'Banana', 'Orange', 'Mango', 'Guava'];
+
+const vegetables =
+    ['Potato', 'Onion', 'Eggplant', 'Radish', 'Carrot', 'Tomato', 'Okra', 'Calabash', 'Cucumber', 'PointedGourd'];
+console.log(vegetables.length);
+
+const movies = ['IronMan', 'Captain', 'Thor', 'JohnWick', 'SpiderMan'];
+
+const oddNumbers = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29,];
+
+const colors = ['Orange', 'White', 'Black', 'Yellow', 'Coffee', 'Pink'];
+console.log(colors.length);
+
+const countryCapitals = ['Dhaka', 'Islamabad', 'London', 'New York', 'Paris'];
+console.log(countryCapitals.length);
