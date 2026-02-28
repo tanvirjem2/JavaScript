f8a7f580fdc9fdcec00ee2021c3a52f7ad74a1ba
tanvirjem2 <tanviranjum046@gmail.com>
2026-02-28 17:45:12 +0600

    Practice - 2 (L6)

---
+++ lesson-06-array/practice2.js
@@ -0,0 +1,26 @@
+// --- Practice - 2 ---
+// --- Problem - 7 (Solved) ---
+
+const friendsName = ['Nayem', 'Sahin', 'Sojib', 'Mashruk'];
+console.log(friendsName[3]);
+
+const books = ['JavaScript', 'Chokher Bali', 'Bonkesh', 'Faluda', 'BCS', 'Tin Goyenda', 'Sherlock Homes'];
+console.log(books[5]);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers[7] = 30;
+console.log(numbers);
+
+const games = ['Free Fire', 'PUBG Mobile', 'Candy Crash', 'Temple Run'];
+games[2] = 'Subway Surface';
+console.log(games);
+
+const familyMembers = ['Shozib', 'Fahim', 'Allo', 'Shafiqul', 'Suma'];
+console.log(familyMembers[3]);
+
+const country = ['Bangladesh', 'USA', 'UK', 'Denmark', 'Pakistan', 'India', 'Sweden'];
+console.log(country[4]);
+
+const onTable = ['Mobile', 'Book', 'Air Pod', 'Medicine',];
+console.log(onTable[7]);
+// The output will be undefined
