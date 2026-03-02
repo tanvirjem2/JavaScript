912f3df1de366374397a8c195d4f58a90c5e4d57
tanvirjem2 <tanviranjum046@gmail.com>
2026-03-03 04:00:03 +0600

    Practice - 3 (L7)

---
+++ lesson-07-loop/practice3.js
@@ -0,0 +1,46 @@
+// --- Practice - 3 ---
+// --- Problem - 5 (Solved) ---
+
+
+let number = 50;
+while (number <= 100) {
+    console.log(number);
+    number++;
+}
+
+
+let num = 5;
+let sum = 0;
+while (num <= 15) {
+    sum = sum + num;
+    num++;
+}
+console.log(sum);
+
+
+let num2 = 1;
+let sum2 = 0;
+while (num2 <= 50) {
+    sum2 = sum2 + num2;
+    num2++
+}
+console.log(sum2);
+
+
+let num3 = 21;
+let sum3 = 0;
+while (num3 <= 50) {
+    sum3 = sum3 + num3;
+    console.log("Sum: " + sum3);
+    num3++;
+}
+console.log(sum3);
+
+
+let num4 = 20;
+let sum4 = 0;
+while (num4 <= 40) {
+    sum4 = sum4 + num4;
+    num4++;
+}
+console.log("Sum: " + sum4);
