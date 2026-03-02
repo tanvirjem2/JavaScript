b672ee3c6a240b53b56b17d15cbe2cc0f5f257b6
tanvirjem2 <tanviranjum046@gmail.com>
2026-03-03 02:04:28 +0600

    Practice - 1 (L7)

---
+++ lesson-07-loop/practice1.js
@@ -0,0 +1,45 @@
+// --- Practice - 1 ---
+// --- Problem - 5 (Solved) ---
+
+const subjects = ['math', 'science', 'physics', 'chemistry', 'biology'];
+for (const subject of subjects) {
+    console.log(subject);
+}
+
+const dishes = ['rice', 'meat', 'chicken', 'vegetable'];
+for (const dish of dishes) {
+    console.log(dish);
+}
+
+const birthYears = [2002, 2008, 2009, 1985, 1967];
+for (const birthYear of birthYears) {
+    console.log(birthYear);
+}
+
+const footballPlayers = [
+    "Lionel Messi",
+    "Cristiano Ronaldo",
+    "Neymar Jr",
+    "Kylian Mbappé",
+    "Kevin De Bruyne",
+    "Luka Modrić",
+    "Virgil van Dijk",
+    "Sergio Ramos",
+    "Robert Lewandowski",
+    "Erling Haaland",
+    "Manuel Neuer"
+];
+for (const footballPlayer of footballPlayers) {
+    console.log(footballPlayer);
+}
+
+const examDates = [
+    "2026-03-10",
+    "2026-03-15",
+    "2026-03-18",
+    "2026-03-22",
+    "2026-03-25"
+];
+for (const examDate of examDates) {
+    console.log(examDate);
+}
