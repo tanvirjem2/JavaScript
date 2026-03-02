bea6906c7fc77f45ff2f6f6b2c1f4c009d149fe9
tanvirjem2 <tanviranjum046@gmail.com>
2026-03-03 05:35:56 +0600

    Practice - 4 (L7)

---
+++ lesson-07-loop/practice4.js
@@ -0,0 +1,22 @@
+// --- Practice - 4 ---
+// --- Problem - 3 (Solved) ---
+
+
+for (let num = 150; num <= 170; num++) {
+    console.log(num);
+}
+
+
+let sum = 0;
+for (let num1 = 31; num1 <= 58; num1++) {
+    sum = sum + num1
+    console.log("Sum:" + sum);
+}
+console.log(sum);
+
+
+let sum1 = 0;
+for (let i = 25; i <= 75; i++) {
+    sum1 = sum1 + i
+}
+console.log("Sum: " + sum1)
