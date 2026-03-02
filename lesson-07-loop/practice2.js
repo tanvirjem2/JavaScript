94d75f10552d4c45d35c1a9b679f4e9f99112568
tanvirjem2 <tanviranjum046@gmail.com>
2026-03-03 02:52:24 +0600

    Practice - 2 (L7)

---
+++ lesson-07-loop/practice2.js
@@ -0,0 +1,32 @@
+// --- Practice - 2 ---
+// --- Problem - 5 (Solved) ---
+
+let sentence = 0;
+while (sentence < 10) {
+    console.log('I regularly practice programming for 3 hours a day.');
+    sentence++;
+}
+
+let number = 1;
+while (number <= 20) {
+    console.log(number);
+    number++
+}
+
+let num = 50;
+while (num <= 100) {
+    console.log(num);
+    num++;
+}
+
+let num1 = 1;
+while (num1 <= 10) {
+    console.log(`3 X ${num1} = ${3 * num1}`);
+    num1++;
+}
+
+let num2 = 110;
+while (num2 <= 111) {
+    console.log(num2 / 2);
+    num2++;
+}
