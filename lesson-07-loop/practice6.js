23babf6dc9e70a805a541b2a640deae7a7707cb9
tanvirjem2 <tanviranjum046@gmail.com>
2026-03-03 17:26:25 +0600

    Practice - 6 (L7)

---
+++ lesson-07-loop/practice6.js
@@ -0,0 +1,49 @@
+// --- Practice - 6 ---
+// --- Problem - 6 (Solved) ---
+
+
+for (let i = 1; i <= 30; i++) {
+    if (i > 15) {
+        break;
+    }
+    console.log(i);
+}
+
+
+for (let i = 1; i <= 40; i++) {
+    if (i % 7 === 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+
+for (let i = 1; i <= 15; i++) {
+    if (i === 9) {
+        continue;
+    }
+    console.log(i);
+}
+
+
+for (let i = 1; i <= 20; i++) {
+    if (i === 12) {
+        continue;
+    }
+    console.log(i);
+}
+
+for (let i = 1; i <= 25; i++) {
+    if (i % 3 === 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+
+for (let i = 91; i <= 120; i++) {
+    if (i % 10 === 0) {
+        break;
+    }
+    console.log(i);
+}
