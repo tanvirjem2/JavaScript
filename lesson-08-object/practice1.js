80be2e3aeeaa0f06f8962090174bfbc0c0025c91
tanvirjem2 <tanviranjum046@gmail.com>
2026-04-05 23:13:48 +0600

    Practice - 1 (L8)

---
+++ lesson-08-object/practice1.js
@@ -0,0 +1,50 @@
+// --- Practice - 1 ---
+// --- Problem - 7 (Solved) ---
+
+const teacher = {
+    name: 'Tanvir',
+    subject: 'Math',
+    age: 23,
+    home: 'Rajshahi',
+}
+
+const tree = {
+    name: 'Malus domestica',
+    height: "5 m",
+    age: 10,
+    fruit: 'Apple'
+}
+
+const cow = {
+    leg: 4,
+    eye: 2,
+    gender: male
+}
+
+const father = {
+    name: 'Shaqul Alam',
+    occupation: 'Police',
+    age: '58',
+    childrenNumber: 3
+}
+
+const motorbike = {
+    brandName: 'Royel Enfield',
+    tireNumber: 2,
+    color: 'Black',
+    highestSpeed: 250
+}
+
+const bird = {
+    name: 'Parot',
+    color: 'green',
+    food: 'rice'
+}
+
+const laptop = {
+    brand: 'Asus',
+    processor: 'Core i7 13650HX',
+    ram: 16,
+    price: 165000,
+    displaySize: 16
+}
